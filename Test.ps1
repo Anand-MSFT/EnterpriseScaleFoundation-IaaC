f16e811e0b77916b31743fd44b7519b78d55d43b
Andy <andypat1980@gmail.com>
2021-08-06 18:42:04 -0500

    Test PS script with different code

--- Test.ps1
+++ Test.ps1
@@ -1,30 +1,46 @@
-#Fetch parameter value from ADOProect-parameter.json file
-$JSONFromFile = Get-Content -Raw -Path .\ADOProject-parameter.json | ConvertFrom-Json
-#Write-Host "ADO Pat Token = " $JSONFromFile.Pat
-Write-Host "ADO Project Name = " $JSONFromFile.ProjectName
-Write-Host "ADO Organization = " $JSONFromFile.Organization
-$adoOrganization = $JSONFromFile.Organization
-$orgUrl = "https://dev.azure.com/$adoOrganization"
-$pat = "$JSONFromFile.Pat"
-$queryString = "api-version=5.1"
+function GetUrl() {
+    param(
+        [string]$orgUrl, 
+        [hashtable]$header, 
+        [string]$AreaId
+    )
 
-Write-Host "ADO Pat Token = " $pat
-# Create header with PAT
-$token = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes(":$($pat)"))
-# $header = @{authorization = "Basic $token"}
-$header = @{authorization = ("Basic $token")}
+    # Area ids
+    # https://docs.microsoft.com/en-us/azure/devops/extend/develop/work-with-urls?view=azure-devops&tabs=http&viewFallbackFrom=vsts#resource-area-ids-reference
+    # Build the URL for calling the org-level Resource Areas REST API for the RM APIs
+    $orgResourceAreasUrl = [string]::Format("{0}/_apis/resourceAreas/{1}?api-preview=5.0-preview.1", $orgUrl, $AreaId)
 
-# Get the list of all projects in the organization
-$projectsUrl = "$orgUrl/_apis/projects?$queryString"
-Write-Host "ProjectsURL : $projectsUrl" 
-function getAllProjectslist{
-    # get and list all projects under current organization
-    Write-Host "Ready to execute REST API request from PS script"
-    $projects = Invoke-RestMethod -Uri $projectsUrl -Method Get -ContentType "application/json" -Headers $header
-    Write-Host $projects | ConvertTo-Json
-    $projects.value | ForEach-Object {
-    Write-Host $_.id $_.name
+    # Do a GET on this URL (this returns an object with a "locationUrl" field)
+    $results = Invoke-RestMethod -Uri $orgResourceAreasUrl -Headers $header
+
+    # The "locationUrl" field reflects the correct base URL for RM REST API calls
+    if ("null" -eq $results) {
+        $areaUrl = $orgUrl
     }
+    else {
+        $areaUrl = $results.locationUrl
+    }
+
+    return $areaUrl
 }
 
-getAllProjectslist
+$orgUrl = "https://dev.azure.com/DemoWebAppOrg"
+$personalToken = "e7awvivq5c5gqzq3dkepooc2dq57uhu2myanh5b25kgfsjcdjsla"
+
+Write-Host "Initialize authentication context" -ForegroundColor Yellow
+$token = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes(":$($personalToken)"))
+$header = @{authorization = "Basic $token"}
+
+# DEMO 1 List of projects
+Write-Host "Demo 1"
+$coreAreaId = "79134c72-4a58-4b42-976c-04e7115f32bf"
+$tfsBaseUrl = GetUrl -orgUrl $orgUrl -header $header -AreaId $coreAreaId
+
+# https://docs.microsoft.com/en-us/rest/api/azure/devops/core/projects/list?view=azure-devops-rest-5.1
+$projectsUrl = "$($tfsBaseUrl)_apis/projects?api-version=5.1"
+
+$projects = Invoke-RestMethod -Uri $projectsUrl -Method Get -ContentType "application/json" -Headers $header
+
+$projects.value | ForEach-Object {
+    Write-Host $_.name
+}

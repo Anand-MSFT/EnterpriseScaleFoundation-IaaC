fa04e7faf5be42e2b2112d786bc33b3cb182c3c2
Andy <andypat1980@gmail.com>
2021-08-06 17:49:44 -0500

    Added code to test ADO Org connectivity through RESTAPI

--- Test.ps1
+++ Test.ps1
@@ -3,8 +3,24 @@ $JSONFromFile = Get-Content -Raw -Path .\ADOProject-parameter.json | ConvertFrom
 Write-Host "ADO Pat Token = " $JSONFromFile.Pat
 Write-Host "ADO Project Name = " $JSONFromFile.ProjectName
 Write-Host "ADO Organization = " $JSONFromFile.Organization
-# foreach($val in $JSONFromFile)
-# {
-#     $JSONFromFile.Pat
-#     Write-Host "id = " $val.id " and name = " $val.name 
-# }
+$adoOrganization = $JSONFromFile.Organization
+$orgUrl = "https://dev.azure.com/$adoOrganization"
+$pat = "$JSONFromFile.Pat"
+$queryString = "api-version=5.1"
+
+# Create header with PAT
+$token = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes(":$($pat)"))
+$header = @{authorization = "Basic $token"}
+
+# Get the list of all projects in the organization
+$projectsUrl = "$orgUrl/_apis/projects?$queryString"
+Write-Host "ProjectsURL : $projectsUrl" 
+function getAllProjectslist{
+    # get and list all projects under current organization
+    Write-Host"Inside getAllProjectlist function, ready to execute command"
+    $projects = Invoke-RestMethod -Uri $projectsUrl -Method Get -ContentType "application/json" -Headers $header
+    Write-Host $projects | ConvertTo-Json
+    $projects.value | ForEach-Object {
+    Write-Host $_.id $_.name
+    }
+}

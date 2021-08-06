d8f9f7ed3c76696ab5af2f64f61de038c21bd7be
Andy <andypat1980@gmail.com>
2021-08-06 15:45:01 -0500

    Updated PS file with required project validation as well as seperate parameter json file

--- ADOProject.ps1
+++ ADOProject.ps1
@@ -1,6 +1,6 @@
 # Define organization base url, PAT and API version variables
-$orgUrl = "https://dev.azure.com/DemoWebAppOrg"
-$pat = "{please provide your ADO PAT Token for authentication}"
+$orgUrl = "https://dev.azure.com/DemoWebAppOrg" # "https://dev.azure.com/$JSONFromFile.ADO-Organization"
+$pat = "{Put your ADO PAT Token here}" # "$JSONFromFile.ADO-PAT"
 $queryString = "api-version=5.1"
 
 # Create header with PAT
@@ -10,13 +10,38 @@ $header = @{authorization = "Basic $token"}
 # Get the list of all projects in the organization
 $projectsUrl = "$orgUrl/_apis/projects?$queryString"
 
-function getAllProjectslist{
+function isProjectExist {
+    param ( [string] $projectName )
     # get and list all projects under current organization
+    $projectExist = $false
     $projects = Invoke-RestMethod -Uri $projectsUrl -Method Get -ContentType "application/json" -Headers $header
     Write-Host $projects | ConvertTo-Json
-    $projects.value | ForEach-Object {
-    Write-Host $_.id $_.name
+    foreach ($val in $projects.value) {
+        if ($val.name -match $projectName)
+        {
+            $projectExist = $true
+            break
+        }
     }
+    return $projectExist;
+}
+
+# Wait for 10 seconds
+# Start-Sleep -s 10
+function checkStatus{
+    param( 
+            [string] $responseId
+         ) 
+    
+    $operationStatusUrl = "$orgUrl/_apis/operations/$($response.id)?$queryString"
+    
+    # Wait for 10 seconds
+    Write-Host "Wait....your request is in progress"
+    Start-Sleep -s 10
+    
+    # Get Operation Status for respective request for Project e.g. creat/delete/update
+    $response = Invoke-RestMethod -Uri $operationStatusUrl -Method Get -ContentType "application/json" -Headers $header
+    Write-Host "Status of your request is :" $response.status
 }
 
 # Create new project named Demo
@@ -25,41 +50,39 @@ $projectName = "ADORESTAPI-Project"
 function createProject{
     $confirmation = Read-Host "Are you sure you want to Create the project $($projectName) (y/n)"
     if ($confirmation.ToLower() -eq 'y') {
-        $createProjectURL = "$orgUrl/_apis/projects?$queryString"
-        $projectJSON = @{name = "$($projectName)"
-                    description = "Azure DevOps REST API Demo"
-                    capabilities = @{
-                        versioncontrol = @{
-                            sourceControlType = "Git"
+        $projectExist = isProjectExist -projectName $projectName
+        if(!$projectExist)
+        {
+            $createProjectURL = "$orgUrl/_apis/projects?$queryString"
+            $projectJSON = @{name = "$($projectName)"
+                        description = "Azure DevOps REST API Demo"
+                        capabilities = @{
+                            versioncontrol = @{
+                                sourceControlType = "Git"
+                            }
+                            processTemplate = @{
+                                # Basic Project
+                                templateTypeId = "b8a3a935-7e91-48b8-a94c-606d37c3e9f2"                           
+                            }
                         }
-                        processTemplate = @{
-                            # Basic Project
-                            templateTypeId = "b8a3a935-7e91-48b8-a94c-606d37c3e9f2"
-                            
-                        }
-                    }
-                    } | ConvertTo-Json
-        Invoke-RestMethod -Uri $createProjectURL -Method Post -ContentType "application/json" -Headers $header -Body ($projectJSON)
-        checkStatus
+                        } | ConvertTo-Json
+            $response = Invoke-RestMethod -Uri $createProjectURL -Method Post -ContentType "application/json" -Headers $header -Body ($projectJSON)
+            $createprojectId = $response.Id
+            if (-not ([string]::IsNullOrEmpty($createprojectId)))
+            {
+                checkStatus -responseId $createprojectId
+            }
+        }
+        else {
+            Write-Host "Project already exist with this name : $projectName"
+        }
     }
     else {
         Write-Host "Project Creation Skipped. Scipt completed."
     }
 }
 
-# Wait for 10 seconds
-# Start-Sleep -s 10
 
-function checkStatus{
-    # Get Operation Status for Create Project
-    # Wait for 10 seconds
-    Write-Host "Wait....your request is in progress"
-    Start-Sleep -s 10
-    
-    $operationStatusUrl = "$orgUrl/_apis/operations/$($response.id)?$queryString"
-    $response = Invoke-RestMethod -Uri $operationStatusUrl -Method Get -ContentType "application/json" -Headers $header
-    Write-Host "Status of your request is :" $response.status
-}
 
 function getProjectDetails{
     # Get detailed project information
@@ -83,13 +106,20 @@ function deleteProject{
 
         # Delete a project
         $deleteURL = "$orgUrl/_apis/projects/$($projectId)?$queryString"
-        Invoke-RestMethod -Uri $deleteURL -Method Delete -ContentType "application/json" -Headers $header
-        checkStatus
+        $response = Invoke-RestMethod -Uri $deleteURL -Method Delete -ContentType "application/json" -Headers $header
+        $deleteprojectId = $response.Id
+        if (-not ([string]::IsNullOrEmpty($deleteprojectId)))
+        {
+            checkStatus -responseId $deleteprojectId
+        }
+        else {
+            Write-Host "Project with name $projectName amd id $deleteprojectId does not exist"
+        }
     } else {
         Write-Host "Project not deleted. Scipt completed."
     }
 }
 
-getAllProjectslist;
+# getAllProjectslist;
 createProject;
 deleteProject;
